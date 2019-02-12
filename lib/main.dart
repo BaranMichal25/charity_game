2c933e9184b1dfe313e8340d840e4edd7ce5a3d4
Michal Baran <baranmichal25@gmail.com>
2019-02-12 08:40:05 +0000

    Added BottomNavigationBar with 4 items

--- lib/main.dart
+++ lib/main.dart
@@ -26,22 +26,67 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    PlaceholderWidget('Explore'),
+    PlaceholderWidget('History'),
+    PlaceholderWidget('Stats'),
+    PlaceholderWidget('More')
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'First Page',
-            ),
-          ],
-        ),
-      ),
+      bottomNavigationBar: _bottomNavigationBar(),
+      body: _children[_currentIndex],
     );
+  }
+
+  Widget _bottomNavigationBar() {
+    return BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: _onTabTapped,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.category),
+            title: new Text('Explore'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.history),
+            title: new Text('History'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.graphic_eq),
+            title: new Text('Stats'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.more_horiz),
+            title: new Text('More'),
+          )
+        ]);
+  }
+
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+}
+
+class PlaceholderWidget extends StatelessWidget {
+  final String title;
+
+  PlaceholderWidget(this.title);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Container(
+      child: Text(title, style: new TextStyle(fontSize: 40)),
+    ));
   }
 }
